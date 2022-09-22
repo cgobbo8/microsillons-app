a7fbfb55f896500f8d0da36853462a683c31558e
cgobbo8 <corentin.gobbo@hotmail.fr>
2022-09-22 13:25:35 +0200

    changement auteur.e.s

--- components/bloc/authorsBlog/index.jsx
+++ components/bloc/authorsBlog/index.jsx
@@ -1,15 +1,16 @@
 import Link from 'next/link';
 import { Author } from './author';
-import styles from './AuthorBlog.module.scss'
+import styles from './AuthorBlog.module.scss';
 
-export const AuthorsBlog = ({authors}) => {
-
-    return (
-        <div className={styles.authors__component}>
-            <h2 className={styles['authors--title']}>Les <span className={styles['authors--title--accent']}>auteur-es</span></h2>
-            <ul className={styles.authors}>
-                {authors.map(author => author?.attributes?.articles?.data[0] && <Author key={author.id} author={author} />)}
-            </ul>
-        </div>
-    )
-}
+export const AuthorsBlog = ({ authors }) => {
+  return (
+    <div className={styles.authors__component}>
+      <h2 className={styles['authors--title']}>
+        Les <span className={styles['authors--title--accent']}>auteur.e.s</span>
+      </h2>
+      <ul className={styles.authors}>
+        {authors.map((author) => author?.attributes?.articles?.data[0] && <Author key={author.id} author={author} />)}
+      </ul>
+    </div>
+  );
+};
