4b9025f3dbdd6be4847a3ec22b0a74a4254327e7
Corentin Gobbo <corentin.gobbo@hotmail.fr>
2023-07-10 22:20:46 +0200

    try revalidate

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,21 +1,19 @@
-import Articles from "../components/bloc/articles";
-import Seo from "../components/bloc/seo";
-import EmblaCarousel from "../components/common/EmblaCarousel";
-import { CarouselArticles } from "../components/common/Flickity";
-import { AssociationSection } from "../components/sections/association";
-import { CompagnonsSection } from "../components/sections/compagnons";
-import { EmissionsSection } from "../components/sections/emissions";
-import { EquipeSection } from "../components/sections/equipeSection";
-import { RadioSection } from "../components/sections/radio";
-import { SeriesSection } from "../components/sections/series";
-import { fetchAPI } from "../lib/api";
-
+import Articles from '../components/bloc/articles';
+import Seo from '../components/bloc/seo';
+import EmblaCarousel from '../components/common/EmblaCarousel';
+import { CarouselArticles } from '../components/common/Flickity';
+import { AssociationSection } from '../components/sections/association';
+import { CompagnonsSection } from '../components/sections/compagnons';
+import { EmissionsSection } from '../components/sections/emissions';
+import { EquipeSection } from '../components/sections/equipeSection';
+import { RadioSection } from '../components/sections/radio';
+import { SeriesSection } from '../components/sections/series';
+import { fetchAPI } from '../lib/api';
 
 const Home = ({ articles, slides, categories, homepage, emissions, series, equipe, compagnons, openContact }) => {
-
   return (
     <div>
-      <Seo seo={{...homepage.attributes.seo, title : homepage.attributes.titrePage }} />
+      <Seo seo={{ ...homepage.attributes.seo, title: homepage.attributes.titrePage }} />
       <div className="">
         <div className="">
           {/* <h1>{homepage.attributes.titrePage}</h1> */}
@@ -31,48 +29,48 @@ const Home = ({ articles, slides, categories, homepage, emissions, series, equip
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
 export default Home;
 
 export async function getStaticProps() {
   // Run API calls in parallel
   const [articlesRes, slidesRes, categoriesRes, homepageRes, emissionsRes, seriesRes, equipesRes, compagnonsRes] = await Promise.all([
-    fetchAPI("/articles", { populate: "*" }),
-    fetchAPI("/articles", { 
+    fetchAPI('/articles', { populate: '*' }),
+    fetchAPI('/articles', {
       pagination: {
         pageSize: 4
-      }, 
-      populate: "*",
-      sort: "publishedAt:DESC"
-     }),
-    fetchAPI("/podcast-types", { populate: "*" }),
-    fetchAPI("/homepage", {
-      populate: {
-        hero: "*",
-        seo: { populate: "*" },
-        association_bloc: { populate: "*", liste : { populate : '*'} },
-        'association_bloc.liste' : { populate: "*" },
-
-        radio_bloc : { populate: "*" },
-        emissions_bloc: { populate: "*" },
-        series_bloc : { populate: "*" },
-        equipe_bloc: { populate: "*" },
       },
+      populate: '*',
+      sort: 'publishedAt:DESC'
     }),
-    fetchAPI("/emissions", {
-      populate: "*"
+    fetchAPI('/podcast-types', { populate: '*' }),
+    fetchAPI('/homepage', {
+      populate: {
+        hero: '*',
+        seo: { populate: '*' },
+        association_bloc: { populate: '*', liste: { populate: '*' } },
+        'association_bloc.liste': { populate: '*' },
+
+        radio_bloc: { populate: '*' },
+        emissions_bloc: { populate: '*' },
+        series_bloc: { populate: '*' },
+        equipe_bloc: { populate: '*' }
+      }
     }),
-    fetchAPI("/series", {
-      populate: "*"
+    fetchAPI('/emissions', {
+      populate: '*'
     }),
-    fetchAPI("/equipes", {
-      populate: "*"
+    fetchAPI('/series', {
+      populate: '*'
     }),
-    fetchAPI("/sponsors", {
-      populate: "*"
+    fetchAPI('/equipes', {
+      populate: '*'
     }),
+    fetchAPI('/sponsors', {
+      populate: '*'
+    })
   ]);
 
   return {
@@ -84,9 +82,8 @@ export async function getStaticProps() {
       emissions: emissionsRes.data,
       series: seriesRes.data,
       equipe: equipesRes.data,
-      compagnons : compagnonsRes.data
+      compagnons: compagnonsRes.data
     },
-    revalidate: 100, 
+    revalidate: 50
   };
 }
-
